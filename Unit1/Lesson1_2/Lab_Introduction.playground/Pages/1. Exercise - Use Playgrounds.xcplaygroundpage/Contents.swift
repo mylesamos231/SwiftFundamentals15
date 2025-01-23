ce0c81fd5f0929195d698948dee217a07c3a2574
mylesamos231 <156014673+mylesamos231@users.noreply.github.com>
2025-01-23 15:41:27 -0500

    Update Contents.swift
    I updated this one as well. Didn't know if both would need to be completed.

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,14 +10,15 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+// Song title and artist
+print("Song Title: Blinding Lights")
+print("Artist: The Weeknd\n")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("I've been tryna call")
+print("I've been on my own for long enough")
+print("Maybe you can show me how to love, maybe")
 /*:
 
  _Copyright © 2023 Apple Inc._
