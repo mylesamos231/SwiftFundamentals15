d8c9d1737c92abba63b6fef6f72da3eedd63b182
mylesamos231 <156014673+mylesamos231@users.noreply.github.com>
2025-01-23 15:19:59 -0500

    Update Contents.swift
    I updated my phrases to the console. And I also included a song title/artist & some lyrics that go with the song.

--- Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -12,11 +12,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Song Title: Blinding Lights")
+print("Artist: The Weeknd\n")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("I've been tryna call")
+print("I've been on my own for long enough")
+print("Maybe you can show me how to love, maybe")
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
