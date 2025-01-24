2e99d909f4018e19ebb325d11ebf4676f7dadf4d
mylesamos231 <156014673+mylesamos231@users.noreply.github.com>
2025-01-24 15:05:23 -0500

    Completion of Lesson 1_4 Page 3

--- Unit1/Lesson1_4/Lab_Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_4/Lab_Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
@@ -1,23 +1,34 @@
 /*:
-## Exercise - Compound Assignment
+ ## Exercise - Compound Assignment
  
  Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
  */
-
-
+var variable = 10
+print(variable)
+variable += 5
+print(variable)
+variable *= 2
+print(variable)
 /*:
- Create a variable called `pennyBank` that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your penny bank.
- 
-- Your neighbor gives you 10 dollars for mowing her lawn
-- You earn 20 more dollars throughout the week doing odd jobs
-- You spend half your money on dinner and a movie
-- You triple what's left in your penny bank by washing windows
-- You spend 3 dollars at a convenience store
- 
- Print the balance of your penny bank after each step.
+ Create a variable called `piggyBank` that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your piggy bank.
+ - Your neighbor gives you 10 dollars for mowing her lawn
+ - You earn 20 more dollars throughout the week doing odd jobs
+ - You spend half your money on dinner and a movie
+ - You triple what's left in your piggy bank by washing windows
+ - You spend 3 dollars at a convenience store
+ Print the balance of your piggy bank after each step.
  */
-
-
-/*:
-[Previous](@previous)  |  page 3 of 8  |  [Next: App Exercise - Counting](@next)
- */
+var piggyBack = 0
+print(piggyBack)
+piggyBack += 10
+print(piggyBack)
+piggyBack += 20
+print(piggyBack)
+piggyBack /= 2
+print(piggyBack)
+piggyBack *= 3
+print(piggyBack)
+piggyBack += 3
+print(piggyBack)
+//[Previous](@previous)  |  page 3 of 8  |  [Next: App Exercise - Counting](@next)
+// */
