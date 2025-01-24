9b3f349ad9494703fc15673b462e2b0c2d5be04a
mylesamos231 <156014673+mylesamos231@users.noreply.github.com>
2025-01-24 03:58:53 -0500

    Completion of Lesson 1_3 Page 5

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
@@ -10,8 +10,11 @@
  
  For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
  */
-
-
+var likes = 128         
+var comments = 45         
+let year = 2022           
+let month = 5             
+let day = 3               
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Fitness Tracker: Constant or Variable?](@next)
  */
