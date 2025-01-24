3250ffb9597f4e2eb6fdf20ad0f14e66645f53a2
mylesamos231 <156014673+mylesamos231@users.noreply.github.com>
2025-01-24 04:37:04 -0500

    Completion of Lesson 1_3 Page 7

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -1,19 +1,26 @@
 /*:
-## Exercise - Types and Type Safety
+ ## Exercise - Types and Type Safety
  
- Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
+ Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-
-
-//:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
-//:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
-//:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var firstDecimal = 32.8
+var secondDecimal = 9.1
 
 /*:
-[Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
+ Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
+var trueOrFalse = true
+print(" No, You can't assign a boolean (true or false) to a decimal number because they are different types.")
+/*:
+ Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var string = "This is a String created by Myles."
+print("You can't assign a string (text) to a decimal number because they are different types.")
+/*:
+ Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var wholeNumber = 10
+print("You can't assign a whole number (integer) to a decimal number")
+
+//[Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
+/*:
