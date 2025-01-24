43cf9576d39acd165291ae33196c8e2d78a3040a
mylesamos231 <156014673+mylesamos231@users.noreply.github.com>
2025-01-24 03:30:37 -0500

    Completion of Lesson 1_3 Page 3

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -1,16 +1,19 @@
 /*:
-## Exercise - Variables
+ ## Exercise - Variables
  
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
-
-
-//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
-
-
-//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
-
-
+var schooling = 2.1
+print(schooling)
+/*:
+ Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+ */
+schooling = 3
+print(schooling)
+/*:
+ Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+ */
+print("Variables, declared with var, can be changed or updated at any time. However, constants, declared with let, are fixed once they’re assigned a value and cannot be modified later. This is why the code compiles when you update schooling, but it would give an error if schooling were a constant.")
 /*:
 [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
  */
