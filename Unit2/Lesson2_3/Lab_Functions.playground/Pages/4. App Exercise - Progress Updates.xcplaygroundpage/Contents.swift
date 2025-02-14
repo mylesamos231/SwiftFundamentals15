22ddc1a0988bae31337affb023410dfadec2efd1
Myles Amos <mylesamos231@gmail.com>
2025-02-14 01:22:42 -0500

    Lesson 2_3 Completion

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,10 +9,37 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
+func progressUpdate(steps: Int, goal: Int) {
+     let percentOfGoal: Double = (Double(steps)/Double(goal)) * 100
+    
+     if percentOfGoal < 10 {
+         print("You're off to a good start.")
+     } else if percentOfGoal < 50 {
+         print("You're almost halfway there!")
+     } else if percentOfGoal < 90 {
+         print("You're almost there!")
+     } else if percentOfGoal > 100 {
+         print("You beat your goal!")
+     }
+}
 
-
+progressUpdate(steps: 50, goal: 10000)
+progressUpdate(steps: 4324, goal: 10000)
+progressUpdate(steps: 7587, goal: 10000)
+progressUpdate(steps: 12543, goal: 10000)
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let requiredSpeed = totalDistance / goalTime
+    let currentSpeed = currentDistance / currentTime
+    if currentSpeed < requiredSpeed {
+        print("You've got to push it just a bit harder!")
+    } else {
+        print("Keep it up!")
+    }
+}
 
+pacing(currentDistance: 9, totalDistance: 34, currentTime: 16, goalTime: 90)
+pacing(currentDistance: 3, totalDistance: 50, currentTime: 9, goalTime: 100)
 
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
@@ -7,10 +7,26 @@
 
  As an example, write a function that only does a portion of what your previous `pacing` function did. This function will be called `calculatePace`. It should take three `Double` arguments called `currentDistance`, `totalDistance`, and `currentTime`, and should return a `Double` that will represent the time at which the user will finish the run based on the user's current distance and time. call the function and print the return value.
  */
+func calculatePace(currentDistance: Double, totalDistance: Double, currentTime: Double) -> Double {
+    let remainingDistance = totalDistance - currentDistance
+    let currentSpeed = currentDistance / currentTime
+    return remainingDistance / currentSpeed
+}
 
-
+var calculatePaceResult = calculatePace(currentDistance: 8, totalDistance: 40, currentTime: 24)
+print(calculatePaceResult)
 //:  Now write a function called `pacing` that takes four `Double` arguments called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. The function should also return a `String`, which will be the message to show the user. The function should call `calculatePace`, passing in the appropriate values, and capture the return value. The function should then compare the returned value to `goalTime` and if the user is on pace return "Keep it up!", and return "You've got to push it just a bit harder!" otherwise. Call the function and print the return value.
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) -> String {
+    let timeRemaining = calculatePace(currentDistance: currentDistance, totalDistance: totalDistance, currentTime: currentTime)
+    if (currentTime + timeRemaining) <= goalTime {
+        return "Keep it up!"
+    } else {
+        return "You've got to push it just a bit harder!"
+    }
+}
 
+var pacingReturnValue = pacing(currentDistance: 3, totalDistance: 6, currentTime: 20, goalTime: 100)
+print(pacingReturnValue)
 
 /*:
  _Copyright © 2023 Apple Inc._
