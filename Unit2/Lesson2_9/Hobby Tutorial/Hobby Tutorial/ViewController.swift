d4bac115d3d1266a9cd4ded55b7a61132990aa48
Myles Amos <mylesamos231@gmail.com>
2025-02-27 12:08:02 -0500

    Lesson 2_9 Hobby Tutorial Completion

---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Myles Amos on 2/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby TutorialTests/Hobby_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hobby_TutorialTests.swift
+//  Hobby TutorialTests
+//
+//  Created by Myles Amos on 2/27/25.
+//
+
+import Testing
+@testable import Hobby_Tutorial
+
+struct Hobby_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
