dc71cb53e054ce431b5cd4f200b8c74116163c5a
Myles Amos <mylesamos231@gmail.com>
2025-03-02 01:43:25 -0500

    Apple Pie Project Completion

---
+++ Unit2/GuidedProject_ApplePie/Apple Pie/Apple PieTests/Apple_PieTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_PieTests.swift
+//  Apple PieTests
+//
+//  Created by Myles Amos on 3/1/25.
+//
+
+import Testing
+@testable import Apple_Pie
+
+struct Apple_PieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
